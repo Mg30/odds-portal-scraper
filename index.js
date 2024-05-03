d85872f7259e8b85965b67bb117c2a1d1ecd874e
gonza <matgonzalez@hotmail.fr>
2024-05-03 14:58:29 +0200

    bump version

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { exportToS3, exportToDir } from './lib/exporters.js'
 
 
 program
-    .version('2.4.2')
+    .version('2.5.0')
     .description('A CLI tool for scraping soccer odds from the odds portal site.')
     .command('help')
     .description('show available commands')
